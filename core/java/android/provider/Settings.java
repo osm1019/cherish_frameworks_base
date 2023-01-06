29923d912daa2e4beddcfec1294ff3dded7a25e7
Ido Ben-Hur <idoybh2@gmail.com>
2023-01-06 22:13:07 +0200

    base: Allow setting wake for notification separately from doze [1/2]
    The strings were misleading. Disabling wake for notifications actually entirely disables doze.
    Add a new setting to configure wake for notification separately and amend doze switch strings accordingly
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12590,6 +12590,12 @@ public final class Settings {
         public static final String GMS_ENABLED = "gms_enabled";
 
         /**
+         * Whether to trigger doze for new notifications
+         * @hide
+         */
+        public static final String DOZE_FOR_NOTIFICATIONS = "doze_for_notifications";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
